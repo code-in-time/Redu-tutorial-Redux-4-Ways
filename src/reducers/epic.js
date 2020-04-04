11e322cbfcf403404917bebcc8ccd4998f3b5b40
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-04-04 05:33:55 +0200

    Updates - still not working

--- src/reducers/epic.js
+++ src/reducers/epic.js
@@ -5,7 +5,7 @@ import getPeople from '../api'
 import { from, Observable, of} from 'rxjs';
 // import 'rxjs'
 // import { Observable } from 'rxjs/Observable';
-import { mergeMap } from 'rxjs/operators';
+import { mergeMap, map, catchError } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
 
 // const fetchUserEpic = action$ =>
@@ -16,24 +16,34 @@ import { ofType } from 'redux-observable';
 //         .catch(error => Observable.of(getDataFailure(error)))
 //       )
 
-const fetchUserEpic = action$ =>
-  action$.pipe(  //fixed
-    ofType(FETCHING_DATA),
-    mergeMap(action => {
-      return from(getPeople())
-        .flatMap(payload => [
-          {
-            type: 'SIGNUP_CONCIERGE_SUCCESS',
-            payload
-          }
-        ])
-        .catch(error =>
-          of({
-            type: 'SIGNUP_CONCIERGE_ERROR',
-            payload: { error }
-          })
-        );
-    })
-  );
+// const fetchUserEpic = action$ =>
+//   action$.pipe(  //fixed
+//     ofType(FETCHING_DATA),
+//     mergeMap(action => {
+//       return from(getPeople())
+//         .flatMap(payload => [
+//           {
+//             type: 'SIGNUP_CONCIERGE_SUCCESS',
+//             payload
+//           }
+//         ])
+//         .catch(error =>
+//           of({
+//             type: 'SIGNUP_CONCIERGE_ERROR',
+//             payload: { error }
+//           })
+//         );
+//     })
+//   );
+
+const fetchUserEpic = action$ => action$.pipe(
+  ofType(FETCHING_DATA),
+  mergeMap(action => getPeople())
+    .pipe(
+      map(response => getDataSuccess(response)),
+      catchError(error => getDataFailure(error)),
+  )
+);
+
 
 export default fetchUserEpic
