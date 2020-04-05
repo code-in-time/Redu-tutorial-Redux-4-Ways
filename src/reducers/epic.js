02e58788dd9f21c462e8443a12859f8834b31e8b
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-04-05 05:35:49 +0200

    RXJS working

--- src/reducers/epic.js
+++ src/reducers/epic.js
@@ -2,48 +2,16 @@ import { FETCHING_DATA } from '../constants'
 import { getDataSuccess, getDataFailure } from './actions'
 import getPeople from '../api'
 
-import { from, Observable, of} from 'rxjs';
-// import 'rxjs'
-// import { Observable } from 'rxjs/Observable';
-import { mergeMap, map, catchError } from 'rxjs/operators';
+import { from, of } from 'rxjs';
+import { map, catchError, mergeMap } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
-
-// const fetchUserEpic = action$ =>
-//   action$.ofType(FETCHING_DATA)
-//     .mergeMap(action =>
-//       Observable.fromPromise(getPeople())
-//         .map(response => getDataSuccess(response))
-//         .catch(error => Observable.of(getDataFailure(error)))
-//       )
-
-// const fetchUserEpic = action$ =>
-//   action$.pipe(  //fixed
-//     ofType(FETCHING_DATA),
-//     mergeMap(action => {
-//       return from(getPeople())
-//         .flatMap(payload => [
-//           {
-//             type: 'SIGNUP_CONCIERGE_SUCCESS',
-//             payload
-//           }
-//         ])
-//         .catch(error =>
-//           of({
-//             type: 'SIGNUP_CONCIERGE_ERROR',
-//             payload: { error }
-//           })
-//         );
-//     })
-//   );
 
 const fetchUserEpic = action$ => action$.pipe(
   ofType(FETCHING_DATA),
-  mergeMap(action => getPeople())
-    .pipe(
-      map(response => getDataSuccess(response)),
-      catchError(error => getDataFailure(error)),
-  )
+  mergeMap(() => from(getPeople()).pipe(
+    map(getDataSuccess),
+    catchError(error => of(getDataFailure(error)))
+  ))
 );
 
-
-export default fetchUserEpic
+export default fetchUserEpic;
